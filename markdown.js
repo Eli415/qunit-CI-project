c4037e8040c87864c48127eda6ad43d8bee551e4
Elizabeth Frey <eli.frey@gmail.com>
2020-10-22 05:00:44 -0700

    - still checking just one element in the array

--- markdown.js
+++ markdown.js
@@ -11,15 +11,50 @@ function quotient(a, b) {
     return a / b;
 }
 
-// Markdown functions
+// MARKDOWN FUNCTIONS
 // Summary / Detail
 function summaryDetail(a, b) {
     return "<details><summary>" + a + "</summary>" + b + "</details>";
 }
-// Summary / Detail
-function summaryDetail(a, b) {
-    return "<details><summary>" + a + "</summary>" + b + "</details>";
-}
+
+// Description List
+
+/*
+function descriptionList([
+    {
+        title: "Images", 
+        description: ".jpg, .gif, .png"
+    },
+    {
+        title: "Styles", 
+        description: ".css"
+    },
+    {
+        title: "Scripts", 
+        description: ".js"
+    },
+    {
+        title: "Documents", 
+        description: ".html"
+    }
+  ]);
+*/
+
+//var theList = []; // Create new array object
+ function descriptionList() {
+    var items = {
+        "images" : ".jpg, .gif, .png",
+        "bar" : 789012,
+        "baz" : 345678,
+        "bat" : 901234
+      }
+      var str = '';
+      for(var index in items) {
+        return( index + " : " + items[index] + "<br />");
+      }
+ };
+
+
 
 // Make the following things (i.e., { sum, subtract } ) 
 // available to the outside world
@@ -28,5 +63,6 @@ module.exports = {
     difference,
     product,
     quotient,
-    summaryDetail
+    summaryDetail,
+    descriptionList
 }; 
--- test/markdown-test.js
+++ test/markdown-test.js
@@ -1,21 +1,16 @@
 //// Set constant variables
 // Include the calculator.js file, which contains functions and exports their returns / results
-const { sum, difference, quotient, product, summaryDetail } = require("../markdown");
+const { sum, difference, quotient, product, summaryDetail, descriptionList } = require("../markdown");
 
 //// These tests use Quinit and its modules to verify certain conditions then print details about passing / failing
 
-//// TESTS THAT PRINT MARKDOWN (In GitHub Details Page)
+//// TESTS FOR MARKDOWN - PRINT TO GITHUB WEB DETAILS
 QUnit.module('Test that writes markdown');
-QUnit.test('Test that writes markdown on fail', assert => {
-   // this is where we write our test
-   // print markdown string from the summaryDetail function
+QUnit.test('Test markdown for Summary Detail', assert => {
+   // write our test & print markdown string
    assert.equal(summaryDetail('My Summary Content', 'My Details Content'), "<details><summary>My Summary Content</summary>My Details Content</details>", 'Make sure your Summary Content and Summary Details match expected output.');
 })
-
-//// TESTS FOR SUM
-QUnit.module('Tests for sum');
-QUnit.test('Sum with two positive numbers', assert => {
-   // this is where we write our test 
-   assert.equal(sum(3, 4), 7, 'make sure 3 + 4 = 7');
-   assert.equal(sum(2, 1), 3, 'make sure 2 + 1 = 3');
+QUnit.test('Test markdown for Summary Detail', assert => {
+   // write our test & print markdown string
+   assert.equal(descriptionList('My Summary Content', 'My Details Content'), "<details><summary>My Summary Content</summary>My Details Content</details>", 'Make sure your Summary Content and Summary Details match expected output.');
 })
